93591ba2cc60e20169ed4451f143bb2269a02fad
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-04-10 11:07:22 +0200

    login is working but ugly

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,10 +5,27 @@ import { Hint } from '@/components/Hint'
 import { NavBar } from '@/components/NavBar'
 import { Vitae } from '@/components/Vitae'
 import { Competence } from '@/components/Competence'
+import { getSession, checkSession, logout } from '../../../lib'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  await checkSession()
+
   return (
     <>
+      <div>
+        <form
+          action={async () => {
+            'use server'
+            await logout()
+            redirect('/')
+          }}
+        >
+          <button type="submit" className="m-6">
+            Logout
+          </button>
+        </form>
+      </div>
       <Hero />
       <Hint />
       <NavBar />
