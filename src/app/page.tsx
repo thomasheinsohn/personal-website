6d741db65c4ab3e79369d7990ff05dc34e547700
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-04-10 20:38:42 +0200

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,10 @@ export default async function Page() {
         <GridPattern x="50%" y="50%" patternTransform="translate(0 60)" />
       </div>
       <div className="absolute top-0 mx-auto flex flex-wrap items-center justify-center gap-2">
-        <h1 className="text-center font-display text-3xl font-extrabold text-slate-900 sm:text-3xl">
+        <h1 className="mt-5 text-center font-display text-3xl font-extrabold text-slate-900 sm:text-3xl">
           This website is currently under construction
         </h1>
-        <Wrench />
+        <Wrench className="lg:mt-5" />
       </div>
       <Container className="flex flex-col items-center py-16 text-center sm:py-20 lg:py-32">
         <h1 className="font-display text-5xl font-extrabold text-slate-900 sm:text-6xl">
